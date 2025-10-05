006baadb613dd79e2ac484474eacbce95464e948
danfin09 <dankfin@gmail.com>
2025-10-05 22:22:04 +0200

    play

--- ex3b.js
+++ ex3b.js
@@ -3,12 +3,29 @@
  */
 
 function getAverage(marks) {
-    let result = 0;
+    if (!Array.isArray(marks)) {
+        throw new Error("You must pass an array of numbers as argument");
+    }
 
+    if (marks.length === 0) {
+        throw new Error("The array cannot be empty");
+    }
+    
     //First, we need to get the sum of all the elements of an array. For each array element, we have to sum the next element.
     for (let i = 0; i < marks.length; i++) {
+        if (typeof marks[i] !== 'number') {
+            throw new Error("You must pass an array of numbers as argument");
+        }
+
+
         result += marks[i]; //No estaba entendiendo como hacer que a cada iteración se sumara el resultado anterior, hasta que encontré el operador de "asignación de adición" y recordé el consejo de Esteban de pensar en la variable "i" como índice. 
     }
+
+    let result = 0;
+    for (let i = 0; i < marks.length; i++) {
+        result += marks[i];
+    }
+
 
     //Create a variable that includes the operation needed to get the average (dividing the sum between the array lenght). *Intenté usar el operador de asignación de división para no crear una variable nueva, pero no se me ocurrió como...
     const average = result / marks.length;
@@ -16,8 +33,6 @@ function getAverage(marks) {
 
     //BONUS: Math.floor() rounds down and return the largest integer less than or equal to a given number
     return Math.floor(average);
-
-
 }
 
 console.log(getAverage([2, 2, 4, 2])); // Debería devolver un 2
@@ -29,4 +44,16 @@ console.log(getAverage([12, 17, 4, 12, 2, 2, 16, 12, 2, 11, 16, 5, 20])); // Deb
 /** EJERCICIO: Gestión de errores. Descomenta una a una y lanza lase implementa excepciones correspondientes */
 
 // console.log(getAverage(12345)); // Error ! You must pass an array of numbers as argument
+console.log("=== valid cases ===");
+try {
+    console.log(getAverage([12345]));
+} catch (error) {
+    console.error(error.message);
+}
+
 // console.log(getAverage([1, 2, 3, "4"])); // Error ! You must pass an array of numbers as argument
+try {
+    console.log(getAverage([1, 2, 3, 4, 5]));
+} catch (error) {
+    console.error(error.message);
+}
